72e8809120f62126d8155985e81c2cb20642a255
Nico Aymet <nico.aymet@atlantis7.es>
2020-05-28 22:47:46 +0200

    first menu utility

--- src/app.js
+++ src/app.js
@@ -1,3 +1,43 @@
-import BootstrapVue from 'bootstrap-vue' //Importing
+import Vue from 'vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+import Verte from 'verte';
+import 'verte/dist/verte.css';
 
-Vue.use(BootstrapVue) // Telling Vue to use this in whole application
+var app = new Vue({
+    el: '#app',
+    components: {Verte},
+    data: {
+        directions: [
+            {value: 'H', text: 'Horizontal [H]'},
+            {value: 'V', text: 'Vertical [V]'},
+            {value: '1', text: 'Oblicua 1 [1]'},
+            {value: '2', text: 'Oblicua 2 [2]'}
+        ],
+        direction: 'H',
+        speed: 50,
+        duration: 30,
+        width: 30,
+        background: 'rgb(0,0,0)',
+        foreground: 'rgb(255,0,0)',
+        sound: false,
+        menu: true,
+        fullscreen: false,
+        isRun: false
+    },
+    methods: {
+        durationFormatter(value) {
+            return value + "s"
+        },
+        speedFormatter(value) {
+            return value + "rpm"
+        },
+        widthFormatter(value) {
+            return value + "px"
+        },
+        reset() {
+            this.isRun = false;
+        }
+    }
+})
