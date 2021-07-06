16dd20600afbd9a13ebba0813eaa23a02dc7a5c5
Nico Aymet <nico.aymet@sysco.biz>
2021-07-06 17:09:15 +0100

    upgrade mix to version 6

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('src/app.js', 'dist/').sass('src/app.scss', 'dist/');
+mix.js('src/app.js', 'dist/').sass('src/app.scss', 'dist/').vue();
 
 // Full API
 // mix.js(src, output);
